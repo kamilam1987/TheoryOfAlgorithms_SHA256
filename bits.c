9d8d413a2c46ff5c7fe45ffa8500f04aaa9817b3
Kamila Michel <g00340498@gmit.ie>
2019-02-14 20:55:29 +0000

    Added bitwise operators

--- bits.c
+++ bits.c
@@ -5,8 +5,19 @@ int main(int arcg, char *argv[]) {
 	uint32_t a = 1;
 	uint32_t b = 2;
 
-	printf("a: %x\n", a);
-	printf("b: %x\n", b);
+	printf("a: %08x\n", a);
+	printf("b: %08x\n", b);
+
+	 /* 
+	  * Bitwise Operators
+	  * References: https://www.tutorialspoint.com/cprogramming/c_bitwise_operators.htm
+	  */
+	printf("&: %08x\n", (a&b));
+	printf("|: %08x\n", (a|b));
+	printf("^: %08x\n", (a^b));
+	printf("~: %08x\n", (~a));	
+	printf("<<: %08x\n", (a<<1));	
+	printf(">>: %08x\n", (a>>1));	
 
 	return 0;
 
