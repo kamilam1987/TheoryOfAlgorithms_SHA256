9bcdae58619a55fccdd2cb42df3d1edd42432c75
Kamila Michel <g00340498@gmit.ie>
2019-02-14 20:23:51 +0000

    create bits file

---
+++ bits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdint.h>
+
+int main(int arcg, char *argv[]) {
+	uint32_t a = 1;
+	uint32_t b = 2;
+
+	printf("a: %x\n", a);
+	printf("b: %x\n", b);
+
+	return 0;
+
+}
