d449b1128a184661758a3bcea4be2634513c18de
Kamila Michel <g00340498@gmit.ie>
2019-02-28 21:48:13 +0000

    Completed steps 3,4 form section 6.2 part

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -57,7 +57,34 @@ void sha256(){
 	// From page 22, W[t] = ...
 	for(t = 16; t < 64; t++)
 		sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
-	
+	// Initialise a, b, c, d, e, f, g, h as er Step 2 from section 6.2
+	a = H[0]; b = H[1]; c = H[2]; d = H[3];
+       	e = H[4]; f = H[5]; g = H[6]; h = H[7];	
+
+	// Step 3 from secion 6.2
+	for (t = 0; t < 64; t++){
+		T1 = h + SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
+		T2 = SIG_0(a) + Maj(a, b, c);
+		h = g;
+		g = f;
+		f = e;
+		e = d + T!;
+		d = c;
+		c = b;
+		b = a;
+		a = T1 + T2;
+	}
+
+	// Step 4
+	H[0] = a + H[0];
+	H[1] = b + H[1];
+	H[2] = c + H[2]
+	H[3] = d + H[3]
+	H[4] = e + H[4]
+	H[5] = f + H[5]
+	H[6] = g + H[6]
+	H[7] = h + H[7]
+
 }
 
 uint32_t rotr(uint32_t n, uint32_t x){
