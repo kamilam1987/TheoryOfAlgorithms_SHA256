4381aa6e18a002ebd34f802ac789052d09a9313c
Kamila Michel <g00340498@gmit.ie>
2019-03-08 16:39:04 +0000

    Padding the message implemented

--- sha256/padfile.c
+++ sha256/padfile.c
@@ -16,6 +16,10 @@ int main(int argc, char *argv[]) {
 	//Declare instance of message block.
 	union msgblock M;
 
+	// Tracks all read bits
+	uint64_t nobits = 0;
+
+	// Current number of bytes(0-64)
 	uint64_t nobytes;
 
 	//Declare a pointer of type file.
@@ -33,13 +37,33 @@ int main(int argc, char *argv[]) {
 	while (!feof(fptr)) {
 		//Outputs 64 bits integer using llu-unsigned long long int
 		nobytes = fread(M.e, 1, 64, fptr);
-		printf("%llu\n", nobytes);
+		nobits = nobits + (nobytes * 8);
+		// 64 -(8+1)
+		if (nobytes < 56) {
+			printf("I've found a block with less then 55 bytes!\n");
+			M.e[nobytes] = 0x80;
+			while(nobytes < 56) {
+				nobytes = nobytes + 1;
+				// Sets all bytes to 0.
+				M.e[nobytes] = 0x00;
+			}
+			// Sets last element to nobits
+			M.s[7] = nobits;
+			// Set last 8 bytes as 64 int
+		}
+		//printf("%llu\n", nobytes);
 
 	}
 	// Reads first character from a  file.
 	// printf("%c\n",fread(&c, 1, 1, fptr));
 
 	fclose(fptr);
+
+	for (int i = 0; i < 64; i++)
+		// Outputs all elements of M  as 64 indyvidual bytes
+		printf("%x ", M.e[i]);
+	printf("\n");
+	
 
 	return 0;
 
