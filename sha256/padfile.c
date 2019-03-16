3a81e81d3a89b2325bda2269e799ba4cf779110c
Kamila Michel <g00340498@gmit.ie>
2019-03-16 16:57:51 +0000

    Completed message padding part

--- sha256/padfile.c
+++ sha256/padfile.c
@@ -11,6 +11,9 @@ union msgblock {
 	uint64_t s[8];
 };
 
+//Staus for padding the message(tracks how the file is reading).
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main(int argc, char *argv[]) {
 
 	//Declare instance of message block.
@@ -19,8 +22,11 @@ int main(int argc, char *argv[]) {
 	// Tracks all read bits
 	uint64_t nobits = 0;
 
-	// Current number of bytes(0-64)
+	// Current number of bytes(0-64) that file reads.
 	uint64_t nobytes;
+
+	// Declares status variable at the start.
+	enum status S = READ;
 
 	//Declare a pointer of type file.
 	FILE* fptr;
@@ -28,32 +34,64 @@ int main(int argc, char *argv[]) {
 	//Opens a file.
 	fptr = fopen(argv[1], "r");
 
+	int i;
+
 	// Test for file nor existing.
 	if (fptr == NULL) {
 		printf("Error! Could not open file\n");
 		exit(-1);
 	}
 	
-	while (!feof(fptr)) {
+	while (S== READ) {
 		//Outputs 64 bits integer using llu-unsigned long long int
 		nobytes = fread(M.e, 1, 64, fptr);
+		printf("Read %2llu bytes\n", nobytes);
+		// Previos number of bytes plus current number of bytes multiply by 8.
 		nobits = nobits + (nobytes * 8);
 		// 64 -(8+1)
 		if (nobytes < 56) {
 			printf("I've found a block with less then 55 bytes!\n");
+			// The first bytes in M that has not been written over.
+			// 80 it's the hex value(0ne followed by seven zeros).
 			M.e[nobytes] = 0x80;
 			while(nobytes < 56) {
 				nobytes = nobytes + 1;
 				// Sets all bytes to 0.
 				M.e[nobytes] = 0x00;
 			}
-			// Sets last element to nobits
+			// Sets last element to nobits(number of bits in orginal message).
 			M.s[7] = nobits;
-			// Set last 8 bytes as 64 int
+			// Sets status to finish
+			S = FINISH;
+		}else if (nobytes < 64){
+			// Needs another message block that gives all zeros
+			S = PAD0;
+			M.e[nobytes] = 0x80;
+			// If can add some of the padding to current block but not all
+			// Pend with one and fill with all zeros
+			while (nobytes < 64) {
+				nobytes = nobytes + 1;
+				M.e[nobytes] = 0x00;
+			}
+		
+		// Checks if it's at the end of the file.
+		}else if (feof(fptr)) {
+			S = PAD1;
 		}
+
 		//printf("%llu\n", nobytes);
 
 	}
+	// If has another message block to deliver.
+	if (S == PAD0 || S == PAD1) {
+		for (i = 0; i < 56; i++)
+			// Adds a block of paddings where the first 448 bits are zeros
+			M.e[i] = 0x00;
+		//Puts the 64 bits big endian int representing the number of bits that were in the oginal message.
+		M.s[7] = nobits;
+	}
+	if (PAD1)
+		M.e[0] = 0x80;
 	// Reads first character from a  file.
 	// printf("%c\n",fread(&c, 1, 1, fptr));
 
